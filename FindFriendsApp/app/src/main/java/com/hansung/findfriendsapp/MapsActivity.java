4e4a88ef36415030e813ddd3fc3ad56993e86844
Jeongjibsa <a01027011628@gmail.com>
2019-11-30 20:59:03 +0900

    add maps api basic code

--- FindFriendsApp/app/src/main/java/com/hansung/findfriendsapp/MapsActivity.java
+++ FindFriendsApp/app/src/main/java/com/hansung/findfriendsapp/MapsActivity.java
@@ -10,7 +10,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+// https://developers.google.com/maps/documentation/android-sdk/start?hl=ko 참고
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
