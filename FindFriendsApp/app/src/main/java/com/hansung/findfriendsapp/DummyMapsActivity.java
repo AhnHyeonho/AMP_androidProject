ab168fa3a6bbe54193bb1e967a7920dbfcdb872b
ahndding <ahndding@gmail.com>
2019-12-09 13:52:52 +0900

    임시 maps 액티비티 수정1

--- FindFriendsApp/app/src/main/java/com/hansung/findfriendsapp/DummyMapsActivity.java
+++ FindFriendsApp/app/src/main/java/com/hansung/findfriendsapp/DummyMapsActivity.java
@@ -2,13 +2,48 @@ package com.hansung.findfriendsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.hansung.findfriendsapp.model.datasource.Repository;
+import com.hansung.findfriendsapp.model.datasource.RepositoryImpl;
+import com.hansung.findfriendsapp.model.datasource.remote.RemoteDataSourceImpl;
 
 public class DummyMapsActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    private String userID;
+    private Button myPageBtn;
+    private Button spotlightBtn;
+    private TextView loginUserUID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy_maps);
+
+        myPageBtn = findViewById(R.id.myPageBtn);
+        spotlightBtn = findViewById(R.id.spotlightBtn);
+        loginUserUID = findViewById(R.id.loginUserUID);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        userID = firebaseAuth.getCurrentUser().getUid();
+
+        loginUserUID.setText("Login ID : " + userID);
+        myPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DummyMapsActivity.this,MyPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
