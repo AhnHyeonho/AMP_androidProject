26fe53cdbb801c518212e9c7655b362f03f1074a
ahndding <ahndding@gmail.com>
2019-12-09 13:03:46 +0900

    임시 maps 액티비티 생성

---
+++ FindFriendsApp/app/src/main/java/com/hansung/findfriendsapp/DummyMapsActivity.java
@@ -0,0 +1,14 @@
+package com.hansung.findfriendsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DummyMapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dummy_maps);
+    }
+}
