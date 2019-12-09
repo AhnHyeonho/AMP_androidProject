fe01f085c1955ad216e1e93324517ee0be197770
ahndding <ahndding@gmail.com>
2019-12-09 14:09:55 +0900

    spotlight 액티비티 수정1

--- FindFriendsApp/app/src/main/java/com/hansung/findfriendsapp/SpotlightActivity.java
+++ FindFriendsApp/app/src/main/java/com/hansung/findfriendsapp/SpotlightActivity.java
@@ -2,13 +2,38 @@ package com.hansung.findfriendsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SpotlightActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    private String userID;
+    private Button backBtn;
+    private Button spotlightBtn;
+    private TextView loginUserUID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spotlight);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        userID = firebaseAuth.getCurrentUser().getUid();
+
+        backBtn = findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DummyMapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
